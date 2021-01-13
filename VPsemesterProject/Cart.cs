bea8983b92c530d7a100ff7a7492f52dc306507f
Usama Khalid <usamakhalidxo0@gmail.com>
2021-01-13 17:12:44 +0500

    basic cart class

---
+++ VPsemesterProject/Cart.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VPsemesterProject
+{
+    struct Item
+    {
+        string brand;
+        int id;
+        int price;
+    }
+    class Cart
+    {
+        List<Item> list;
+        Cart()
+        {
+            list = new List<Item>();
+        }
+        void remove(Item item)
+        {
+            list.Remove(item);
+        }
+        void add(Item item)
+        {
+            list.Add(item);
+        }
+    }
+}
